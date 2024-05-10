f0ced3d2743c8e5a7a8e57a7b65a5a32f4c52e4a
bzf <sea@bzf.com>
2024-05-10 16:06:19 +0800

    first commit

---
+++ src/main/java/User.java
@@ -0,0 +1,20 @@
+public class User {
+    private String name;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
